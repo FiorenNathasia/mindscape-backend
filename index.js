0e224a51d6bb43b53c5ef2ea808aa3004c15f85d
FiorenNathasia <fiorennathasia@gmail.com>
2025-02-17 16:38:21 +0000

    Finish verify middleware

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-const authRouter = require("./routes/auth");
 const app = express();
+const verify = require("./middleware/verify");
+const authRouter = require("./routes/auth");
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/auth", authRouter);
+app.use(verify);
 
 app.listen(3030, () => {
   console.log("server listening on 3030");
---
+++ middleware/verify.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+function verify(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    res.send(400).send({ message: "There is no token!" });
+  }
+
+  try {
+    const token = authHeader.split(" ")[1];
+    const userId = jwt.verify(token, "mySecretKey").id;
+    res.locals.id = userId;
+    next();
+  } catch (error) {
+    return res.status(403).send({ message: "Token is invalid!" });
+  }
+}
+
+module.exports = verify;
